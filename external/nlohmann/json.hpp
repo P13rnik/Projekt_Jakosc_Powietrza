67059970f8fa847be86d499662699f8b9e620311
P13rnik <szymon.zawadzki@opoczta.pl>
2026-04-20 18:09:30 +0200

    Create json.hpp

---
+++ external/nlohmann/json.hpp
@@ -0,0 +1,4 @@
+// Pobierz oryginalny plik z:
+// https://github.com/nlohmann/json/releases/latest/download/json.hpp
+// i zastąp ten plik.
+// W CMakeLists.txt jest już dodany include_directories dla tego katalogu.
